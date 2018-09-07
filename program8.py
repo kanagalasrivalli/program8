6582de4abf343b6e4330527ec1b29de6d74ab52b
kanagalasrivalli <41742295+kanagalasrivalli@users.noreply.github.com>
2018-09-07 14:27:59 +0530

    Create program8.py

---
+++ program8.py
@@ -0,0 +1,7 @@
+def smallest_num_in_list( list ):
+    min = list[ 0 ]
+    for a in list:
+        if a < min:
+            min = a
+    return min
+print(smallest_num_in_list([5,3,2,1,7,6,10,8,9]))
